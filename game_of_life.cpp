0a585653f7a3e752acfa3c1e432d972a099fcc2c
Everett Gaius Vergara <evergara@shinra.com.ph>
2022-04-06 21:03:38 +0800

    show creatures on screen

--- game_of_life.cpp
+++ game_of_life.cpp
@@ -1,3 +1,4 @@
+#include <iostream>
 #include <vector>
 #include <algorithm>
 #include "TextImage.h"
@@ -18,7 +19,14 @@ auto main(int argc, char **argv) -> int {
     Creatures creatures = spawner(area, N);
 
     // Plot Creatures to Screen
-    
+    screen.fill_text('.');
+    screen.fill_color(7);
+    for (auto &c : creatures) {
+        screen.set_text(c, 'x');
+        screen.set_color(c, 1);
+    }
+
+    screen.show();
     
 
 }
@@ -35,11 +43,14 @@ auto spawner(const Area &area, const Dim &N) -> Creatures {
         std::swap(creature_ixs[i], creature_ixs[rand() % area()]);
 
     Creatures creatures;
-    for (Dim i = 0; i < N; ++i)
+    for (Dim i = 0; i < N; ++i) {
+        std::cout << "x: " << static_cast<Dim>(creature_ixs[i] % area.w) << " y: " << static_cast<Dim>(creature_ixs[i] / area.w) << "\n";
         creatures.emplace_back(
             Point{
                 static_cast<Dim>(creature_ixs[i] % area.w), 
                 static_cast<Dim>(creature_ixs[i] / area.w)});
+    }
     
+    //exit(0);
     return creatures;
 }
