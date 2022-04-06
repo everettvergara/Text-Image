e6f81a6cf125153b264fa045e487a4e410fc8eec
Everett Gaius Vergara <evergara@shinra.com.ph>
2022-04-06 20:23:26 +0800

    added game of life sample

---
+++ game_of_life.cpp
@@ -0,0 +1,16 @@
+#include <vector>
+#include "TextImage.h"
+
+
+// Game of Life Automata
+using namespace g80;
+using Space = std::vector<Point>;
+
+auto main(int argc, char **argv) -> int {
+
+    TextImage screen({140, 50});
+
+    
+    
+
+}
