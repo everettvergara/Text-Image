62e2e9bfc34b1c7e035d7de1ae82e34a964b7014
Everett Gaius Vergara <evergara@shinra.com.ph>
2022-04-07 19:57:46 +0800

    added to spawn

--- game_of_life.cpp
+++ game_of_life.cpp
@@ -31,8 +31,9 @@ auto spawner(const Area &area, const Dim &N) -> Creatures;
 
 auto neighbor_count(Creatures &creatures, Creature &creature) -> Dim;
 auto find_creatures_with_single_or_ge_four(Creatures &creatures, const Area &area) -> Creatures;
-
+auto find_creatures_with_two_or_three(Creatures &creatures, const Area &area) -> Creatures;
 auto kill_creatures(const Creatures &creatures_to_kill, Creatures &creatures) -> void;
+auto spawn_creatures(const Creatures &creatures_to_spawn, Creatures &creatures) -> void;
 
 auto main(int argc, char **argv) -> int {
 
@@ -49,9 +50,11 @@ auto main(int argc, char **argv) -> int {
             screen.set_text(c, ' ');
 
         // Execute Policies
-
-        Creatures to_kill = find_creatures_with_single_or_ge_four(creatures, area, 2);
+        Creatures to_kill = find_creatures_with_single_or_ge_four(creatures, area);
         kill_creatures(to_kill, creatures);
+
+        // Creatures to_spawn = find_creatures_with_two_or_three(creatures, area);
+        // spawn_creatures(to_spawn, creatures);
 
         // Render
         for (auto &c : creatures) {
@@ -72,6 +75,11 @@ auto main(int argc, char **argv) -> int {
 auto kill_creatures(const Creatures &creatures_to_kill, Creatures &creatures) -> void {
     for (auto &c : creatures_to_kill)
         creatures.erase(c);
+}
+
+auto spawn_creatures(const Creatures &creatures_to_spawn, Creatures &creatures) -> void {
+    for (auto &c : creatures_to_spawn)
+        creatures.insert(c);
 }
 
 auto neighbor_count(const Creatures &creatures, const Area &area, const Creature &creature) -> Dim {
@@ -98,17 +106,54 @@ auto neighbor_count(const Creatures &creatures, const Area &area, const Creature
     return neighbor;
 }
 
+
 auto find_creatures_with_single_or_ge_four(Creatures &creatures, const Area &area) -> Creatures {
 
     Creatures to_kill;
-
-    to_kill.reserve(area());
+    to_kill.reserve(area());    // N can be optimized
     for (auto &creature : creatures) {
         Dim neighbors = neighbor_count(creatures, area, creature);
-        if (neighbors <= 2 || neighbors >= 4) 
+        if (neighbors <= 1 || neighbors >= 4) 
             to_kill.insert(creature);
     }
     return to_kill;
+}
+
+// Blank cell with two three neigbors survives
+auto find_creatures_with_two_or_three(Creatures &creatures, const Area &area) -> Creatures {
+
+    Creatures to_test;
+    
+    to_test.reserve(area());    // N can be optimized;
+    for (auto &creature : creatures) {
+        Dim top = creature - area.w;
+        Dim upper_left = top - 1;
+        Dim upper_right = top + 1;
+        Dim left = creature - 1;
+        Dim right = creature + 1;
+        Dim bottom = creature + area.w;
+        Dim bottom_left = bottom - 1;
+        Dim bottom_right = bottom + 1;    
+
+        if (top >= 0 && creatures.find(top) == creatures.end()) to_test.insert(top);
+        if (upper_left >= 0 && creatures.find(upper_left) == creatures.end()) to_test.insert(upper_left);
+        if (upper_right >= 0 && creatures.find(upper_right) == creatures.end()) to_test.insert(upper_right);
+        if (left >= 0 && creatures.find(left) == creatures.end()) to_test.insert(left);
+        if (right < area() && creatures.find(right) == creatures.end()) to_test.insert(right);
+        if (bottom < area() && creatures.find(bottom) == creatures.end()) to_test.insert(bottom);
+        if (bottom_left < area() && creatures.find(bottom_left) == creatures.end()) to_test.insert(bottom_left);
+        if (bottom_right < area() && creatures.find(bottom_right) == creatures.end()) to_test.insert(bottom_right);
+    }
+
+    Creatures to_spawn;
+    to_spawn.reserve(area());   // N can be optimized;
+    for (auto &creature : to_test) {
+        Dim neighbors = neighbor_count(creatures, area, creature);
+        if (neighbors == 2 || neighbors == 3) 
+            to_spawn.insert(creature);
+    }
+
+    return to_spawn;
 }
 
 auto spawner(const Area &area, const Dim &N) -> Creatures {
