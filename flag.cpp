426b4ca66b2c14c252f4fe2785d498daba24a0d5
Everett Gaius Vergara <evergara@shinra.com.ph>
2022-04-12 19:39:54 +0800

    refactor draw flag

--- flag.cpp
+++ flag.cpp
@@ -22,55 +22,29 @@ namespace this_thread = std::this_thread;
 using TimePointSysClock = chr::time_point<chr::system_clock>;
 using SysClock = chr::system_clock;
 
-// Game configurations 
+// Animation configurations 
 constexpr Dim FPS = 15;
 constexpr Dim MSPF = 1000 / FPS;
 
+constexpr Dim wave_height = 6;
+constexpr Dim flag_height = 30;
+constexpr Dim flag_width = 120;
+constexpr double waves = 8.0;
+
 auto delayer(TimePointSysClock start) -> void;
 auto is_key_pressed() -> int;
-auto draw_flag(TextImage &flag) -> void;
+auto draw_flag(TextImage &screen, TextImage &flag) -> void;
 
 auto main(int argc, char **argv) -> int {
-    
-    const Dim wave_height = 6;
-    const Dim flag_height = 30;
-    const Dim flag_width = 120;
-    const double waves = 8.0;
-    
+
     TextImage screen({flag_width, flag_height + wave_height + 5}, 7, ' ', ON);
     TextImage flag({flag_width, flag_height}, 7, '.', ON);
-    
-    
-    // Draw the flag
-    flag.fill_text("BayangmagiliwPerlasngSilangananAlabngpusoSadibdibmoybuhayLupangHinirangDuyankangmagitingSamanlulupigDikapasisiilSadagatatbundokSsimoyatsalangitmongbughawMaydilagangtulaAtawitsapaglayangminamahalAngkislapngwatawatmoyTagumpaynanagniningningAngbituinatarawniyaKailanpamaydimagdidilimLupangarawngluwalhatitpagsintaBuhayaylangitsapilingmoAmingligayana'pagmaymangaapiAngmamatayngdahilsaiyo");
-    // flag.fill_text("Bayang magiliw Perlas ng Silanganan Alab ng puso Sa dibdib moy buhay Lupang Hinirang Duyan ka ng magiting  Sa manlulupig Di ka pasisiil Sa dagat at bundok Sa simoy at sa langit mong bughaw  May dilag ang tula  At awit sa paglayang minamahal Ang kislap ng watawat moy Tagumpay na nagniningning Ang bituin at araw niya  Kailan pa may di magdidilim Lupa ng araw ng luwalhatit pagsinta Buhay ay langit sa piling mo  Aming ligaya na pag may mang-aapi Ang mamatay ng dahil sa iyo.");
+    draw_flag(screen, flag);
 
-    for (Dim i = 0; i < 15; ++i) {
-        for (Dim j = 1 + i * 3; j < flag_width; ++j) {
-            flag.set_color(flag.index({j, i}), 4);
-            flag.set_color(flag.index({j, static_cast<Dim>(flag_height - i - 1)}), 1);
-        }
-    }
-
-    flag.gfx_circle_color({14, 15}, 6, 3);
-    flag.gfx_fill_with_color_border({14, 15}, 'X', 3, ON);
-    for (double i = 0.0; i < 2.0 * M_PI; i += 2.0 * M_PI / 8.0) {
-        flag.set_color({static_cast<Dim>(14.0 + 7.0 * cos(i)), static_cast<Dim>(15.0 + 7.0 * sin(i))}, 3);
-        flag.set_color({static_cast<Dim>(14.0 + 8.0 * cos(i)), static_cast<Dim>(15.0 + 8.0 * sin(i))}, 3);
-    }
-
-    // Stars
-    flag.gfx_line_color({3,3}, {5, 5}, 3);
-    flag.gfx_line_color({3,5}, {5, 3}, 3);
-    flag.gfx_line_color({3, flag_height - 3 - 1}, {5, flag_height - 5 - 1}, 3);
-    flag.gfx_line_color({3, flag_height - 5 - 1}, {5, flag_height - 3 - 1}, 3);
-    flag.gfx_line_color({33,14}, {35, 16}, 3);
-    flag.gfx_line_color({33,16}, {35, 14}, 3);
-
+    // Generate Waves
     Dim x = 0;
     std::array<Dim, flag_width> y;
     std::array<Dim, flag_width> yn;
-
     double inc = waves * M_PI / flag_width;
     for (double a = 0; a < waves * M_PI; a += inc) {
         y[x] = (wave_height / 2.0) - sin(a) * (wave_height / 2.0 - 1) - 1;
@@ -81,9 +55,11 @@ auto main(int argc, char **argv) -> int {
     do {
 
         TimePointSysClock start_delay_timer {SysClock::now()};
-        
+
+        // Erase
         screen.fill_text(" ");
 
+        // Update
         flag.xlat_rotate_right(flag_width - 1, TEXT);
         for (Dim x = 0; x < flag_width; ++x) {
             TextImage vertical_line = flag.get_image({{x, 0}, {1, flag_height}});
@@ -98,6 +74,7 @@ auto main(int argc, char **argv) -> int {
             } 
         }
 
+        // Render
         screen.show();
 
         delayer(start_delay_timer);
@@ -132,6 +109,30 @@ auto is_key_pressed() -> int {
     return bytesWaiting;
 }
 
-auto draw_flag(TextImage &flag) -> void {
+auto draw_flag(TextImage &screen, TextImage &flag) -> void {
+    
+    // Flag Text
+    flag.fill_text("BayangmagiliwPerlasngSilangananAlabngpusoSadibdibmoybuhayLupangHinirangDuyankangmagitingSamanlulupigDikapasisiilSadagatatbundokSsimoyatsalangitmongbughawMaydilagangtulaAtawitsapaglayangminamahalAngkislapngwatawatmoyTagumpaynanagniningningAngbituinatarawniyaKailanpamaydimagdidilimLupangarawngluwalhatitpagsintaBuhayaylangitsapilingmoAmingligayana'pagmaymangaapiAngmamatayngdahilsaiyo");
+    for (Dim i = 0; i < 15; ++i) {
+        for (Dim j = 1 + i * 3; j < flag_width; ++j) {
+            flag.set_color(flag.index({j, i}), 4);
+            flag.set_color(flag.index({j, static_cast<Dim>(flag_height - i - 1)}), 1);
+        }
+    }
 
+    // Sun
+    flag.gfx_circle_color({14, 15}, 6, 3);
+    flag.gfx_fill_with_color_border({14, 15}, 'X', 3, ON);
+    for (double i = 0.0; i < 2.0 * M_PI; i += 2.0 * M_PI / 8.0) {
+        flag.set_color({static_cast<Dim>(14.0 + 7.0 * cos(i)), static_cast<Dim>(15.0 + 7.0 * sin(i))}, 3);
+        flag.set_color({static_cast<Dim>(14.0 + 8.0 * cos(i)), static_cast<Dim>(15.0 + 8.0 * sin(i))}, 3);
+    }
+
+    // Stars
+    flag.gfx_line_color({3,3}, {5, 5}, 3);
+    flag.gfx_line_color({3,5}, {5, 3}, 3);
+    flag.gfx_line_color({3, flag_height - 3 - 1}, {5, flag_height - 5 - 1}, 3);
+    flag.gfx_line_color({3, flag_height - 5 - 1}, {5, flag_height - 3 - 1}, 3);
+    flag.gfx_line_color({33,14}, {35, 16}, 3);
+    flag.gfx_line_color({33,16}, {35, 14}, 3);
 }
