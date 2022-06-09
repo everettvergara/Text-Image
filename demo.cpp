e1156bad6aacb6b8c8c2e7b7c2a31f43363da9a8
Everett Gaius Vergara <evergara@shinra.com.ph>
2022-06-10 07:44:45 +0800

    added run, preprocess, delayer, event and update in the text video anim class

--- demo.cpp
+++ demo.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 
-#include "text_image.hpp"
+// #include "text_image.hpp"
+#include "text_video_anim.hpp"
 
 using namespace g80;
 
@@ -8,11 +9,16 @@ using namespace g80;
 
 auto main(const int argc, const char *argv[]) -> int {
 
-    text_image<int16_t, uint16_t> ti(140, 30, 7, '`', ON);
-    ti.gfx_line_text(0,0,139,29, '*');
-    ti.gfx_circle_text(70, 15, 10, '*');
-    ti.gfx_fill_with_text_border(70, 15, 1, '*', ON);
-    // ti.gfx_fill_text(70, 15, '*');
-    ti.show();
+    // text_image<int16_t, uint16_t> ti(140, 30, 7, '`', ON);
+    // ti.gfx_line_text(0,0,139,29, '*');
+    // ti.gfx_circle_text(70, 15, 10, '*');
+    // ti.gfx_fill_with_text_border(70, 15, 1, '*', ON);
+    // // ti.gfx_fill_text(70, 15, '*');
+    // ti.show();
+
+    text_video_anim<int16_t, uint16_t> tva(130, 40);
+
+    tva.preprocess();
+    tva.run();
 
 }
---
+++ include/text_video_anim.hpp
@@ -0,0 +1,118 @@
+#ifndef TEXT_VIDEO_ANIM_HPP
+#define TEXT_VIDEO_ANIM_HPP
+
+#include <chrono>
+#include <thread>
+#include "text_image.hpp"
+
+// Includes for is_key_pressed
+#include <cstdio>
+#include <sys/select.h>
+#include <sys/ioctl.h>
+#include <termios.h>
+
+namespace g80 {
+
+    auto is_key_pressed() -> int {
+        static const int STDIN = 0;
+        static bool initialized = false;
+
+        if (!initialized) {
+            termios term;
+            tcgetattr(STDIN, &term);
+            term.c_lflag &= ~(ICANON | ECHO);
+            tcsetattr(STDIN, TCSANOW, &term);
+            setbuf(stdin, NULL);
+            initialized = true;
+        }
+
+        int bytes_waiting;
+        ioctl(STDIN, FIONREAD, &bytes_waiting); 
+        return bytes_waiting;
+    }
+
+    template<typename int_type, typename uint_type>
+    class text_video_anim {
+    
+    /**
+     * Constructors, Assignments and 
+     * Destructors 
+     * 
+     */
+
+    public:
+
+        text_video_anim(const uint_type w, const uint_type h, const color c = 7, const text t = ' ', const mask_bit m = ON) :
+            timg_(w, h, c, t, m) {}
+
+        virtual ~text_video_anim() = default;
+
+    /**
+     * Getters
+     * 
+     */
+    
+    public:
+
+        inline auto data() -> text_image<int_type, uint_type> & {
+            return timg_;
+        }
+
+        inline auto cdata() const -> const text_image<int_type, uint_type> & {
+            return timg_;
+        }
+
+    /**
+     * User overridable functions 
+     * 
+     */
+
+    protected:
+
+        virtual auto update() -> bool {return true;}
+        
+        virtual auto event() -> bool {
+            if (is_key_pressed()) {
+                is_running_ = false;
+            }
+            return is_running_;
+        }
+
+    private:
+
+        auto delayer(std::chrono::time_point<std::chrono::system_clock> &start) -> void {
+            std::chrono::time_point<std::chrono::system_clock> end {std::chrono::system_clock::now()};
+            uint_type delay = MSPF_ - static_cast<uint_type>(std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count());
+            if (delay > 0) std::this_thread::sleep_for(std::chrono::milliseconds(delay));
+        }
+
+    public:
+
+        virtual auto preprocess() -> bool {return true;}
+        
+        virtual auto run() -> bool {
+            
+            do {
+                std::chrono::time_point<std::chrono::system_clock> start {std::chrono::system_clock::now()};
+                
+                if (event()) {update(); delayer(start);}
+
+            } while(is_running_);
+            
+            return true;
+        }
+
+
+
+    protected:
+
+    // TODO: consoder using literal suffix for width and height
+    // i.e. 10d d -> double the width for conversion factor
+        uint_type FPS_, MSPF_;
+        bool is_running_{false};
+        text_image<int_type, uint_type> timg_;
+        
+    };
+}
+
+#endif 
