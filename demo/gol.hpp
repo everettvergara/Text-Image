bfcf363aacbe6a07d99d7d7a5910c9eb0f5e7321
Everett Gaius Vergara <evergara@shinra.com.ph>
2022-06-10 22:51:02 +0800

    misc edit

--- demo/gol.hpp
+++ demo/gol.hpp
@@ -48,15 +48,12 @@ constexpr uint_type SCREEN_SIZE = SCREEN_WIDTH * SCREEN_HEIGHT;
 constexpr uint_type FPS = 15;
 
 // Creature List: 
+// Use array instead of unordered_list for optimization purposes
 
-// Creature, Count (could be merged to Creature List)
-// Creature: [creature_id, count]: {0, 1}
-using creature_ids = std::bitset<SCREEN_SIZE>;
+using creature_ids = std::array<uint_type, SCREEN_SIZE>;
+using creature_id_exists = std::bitset<SCREEN_SIZE>;
+using creature_ids_ix = std::array<int_type, 9>;
 using creature_ids_neigh_count = std::array<uint_type, SCREEN_SIZE>;
-
-// // Org creatures list by count (9 possible counts: 0 - 8)
-// creature_id: 0, 1, 2, 3, ... n-3, n-2, n-1
-// using creatureid_list = std::unordered_set<uint_type>;
 using neigh_counts_creature_ids = std::array<std::array<uint_type, SCREEN_SIZE>, 9>;
 using neigh_counts_ix = std::array<int_type, 9>;
 
@@ -87,8 +84,40 @@ private:
         return all_creature_ids;
     }
 
-    auto update_creature(uint_type creature_id, uint_type neigh_count) -> void {
+    auto neighbor_count(const uint_type creature_id) -> uint_type {
+        
+        uint_type neighbor = 0;
 
+        uint_type top = creature_id - screen_.width();
+        uint_type upper_left = top - 1;
+        uint_type upper_right = top + 1;
+        uint_type left = creature_id - 1;
+        uint_type right = creature_id + 1;
+        uint_type bottom = creature_id + screen_.width();
+        uint_type bottom_left = bottom - 1;
+        uint_type bottom_right = bottom + 1;    
+
+        if (creature_id_exists_[top]) ++neighbor;
+        if (creature_id_exists_[upper_left]) ++neighbor;
+        if (creature_id_exists_[upper_right]) ++neighbor;
+        if (creature_id_exists_[left]) ++neighbor;
+        if (creature_id_exists_[right]) ++neighbor;
+        if (creature_id_exists_[bottom]) ++neighbor;
+        if (creature_id_exists_[bottom_left]) ++neighbor;
+        if (creature_id_exists_[bottom_right]) ++neighbor;
+
+        return neighbor;
+    }
+
+
+    auto update_creature(uint_type creature_id) -> void {
+
+        // creature_ids_neigh_count_ = neighbor_c
+        // if (!creature_id_exists_[creature_id]) {
+        //     creature_id_exists_[creature_id] = true;
+        // } else {
+
+        // }
         // auto f = creature_id_neigh_count_.find(creature_id);
         // if (f == creature_id_neigh_count_.end()) {
         //     creature_id_neigh_count_.insert(creature_id, neigh_count);
@@ -104,13 +133,14 @@ public:
 
     auto preprocess() -> bool {
 
-        creature_ids_.reset();
-        memset(creature_ids_neigh_count_.data(), 0, sizeof(uint_type) * SCREEN_SIZE);
-        memset(neigh_counts_creature_ids_.data(), 0, sizeof(uint_type) * SCREEN_SIZE);
+        creature_id_exists_.reset();
+        memset(neigh_counts_ix.data(), ~0, sizeof(int_type) * 9);
         memset(neigh_counts_ix.data(), ~0, sizeof(int_type) * 9);
         
         std::vector<uint_type> all_shuffled_creatureids = preprocess_random_creatures();
-        for (uint_type i = 0; i < N_; ++i) creature_ids_.set(all_shuffled_creatureids[i], true);
+        for (uint_type i = 0; i < N_; ++i) {
+            creature_id_exists_[all_shuffled_creatureids[i]] = true;
+        }
     }
 
     auto update() -> bool {
@@ -122,6 +152,8 @@ public:
 private:
     uint_type N_;
     creature_ids creature_ids_;
+    creature_ids_ix creature_ids_ix_;
+    creature_id_exists creature_id_exists_;
     creature_ids_neigh_count creature_ids_neigh_count_;
     neigh_counts_creature_ids neigh_counts_creature_ids_;
     neigh_counts_ix neigh_counts_ix;
